7df5c067c34cc4b08d75037d612fbd583a54bc15
Ciro S. Costa <ciro.costa@liferay.com>
2018-01-16 12:23:16 -0200

    improv

--- background.js
+++ background.js
@@ -4,14 +4,15 @@ const filter = {
     '*://facebook.com/*',
     '*://news.ycombinator.com/*',
     '*://twitter.com/*',
+    '*://github.com/',
   ],
 };
 
 const opt = ['blocking'];
 
 window.chrome.webRequest.onBeforeRequest.addListener(
-  (page) => {
-    console.log("page blocked - " + page.url);
+  page => {
+    console.log('page blocked - ' + page.url);
 
     return {
       cancel: true,
